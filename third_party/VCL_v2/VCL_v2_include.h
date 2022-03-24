236d445ade083f78e5be18a9b635f21a66620d24
KKSSainz <victor.trnka@gmail.com>
2022-03-24 09:24:19 +0100

    add inclue file that turn off warnings for VCL

---
+++ third_party/VCL_v2/VCL_v2_include.h
@@ -0,0 +1,15 @@
+// save diagnostic state
+#pragma GCC diagnostic push 
+
+// turn off the specific warning.
+#pragma GCC diagnostic ignored "-Wall"
+
+#include "vectorclass.h"
+
+#include "vectormath_exp.h"
+#include "vectormath_hyp.h"
+#include "vectormath_trig.h"
+
+
+// turn the warnings back on
+#pragma GCC diagnostic pop
