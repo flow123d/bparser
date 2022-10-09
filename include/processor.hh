b382bc54e1fec0c15ddad4af629b193bc2d113a1
KKSSainz <victor.trnka@gmail.com>
2022-10-09 16:26:34 +0200

    test with create processor

--- include/processor.hh
+++ include/processor.hh
@@ -890,7 +890,6 @@ ProcessorBase * create_processor_(ExpressionDAG &se, uint vector_size,  uint sim
     uint simd_bytes1 = sizeof(VCLVec);
     // std::cout << simd_bytes1 << "!=" << simd_bytes << "\n";
     BP_ASSERT(simd_bytes1 == simd_bytes);
-	simd_bytes = simd_bytes1;
     uint vec_size = (vector_size / simd_size);
     uint est = 
             align_size(simd_bytes, sizeof(Processor<Vec<VCLVec>>)) +	// always 88
@@ -911,6 +910,31 @@ ProcessorBase * create_processor_(ExpressionDAG &se, uint vector_size,  uint sim
     return arena->create<Processor<Vec<VCLVec>>>(arena, se, vec_size);
 }
 
+inline ProcessorBase * create_processor_4(ExpressionDAG &se, uint vector_size,  uint simd_size, ArenaAllocPtr arena) {
+    uint simd_bytes = sizeof(double) * simd_size;
+    ExpressionDAG::NodeVec & sorted_nodes = se.sort_nodes();
+    uint simd_bytes1 = sizeof(Vec4d);
+    // std::cout << simd_bytes1 << "!=" << simd_bytes << "\n";
+    BP_ASSERT(simd_bytes1 == simd_bytes);
+    uint vec_size = (vector_size / simd_size);
+    uint est = 
+            align_size(simd_bytes, sizeof(Processor<Vec<Vec4d>>)) +	// always 88
+            align_size(simd_bytes, sizeof(uint) * vector_size) +
+            align_size(simd_bytes, se.temp_end * sizeof(Vec<Vec4d>)) +    // temporaries
+            align_size(simd_bytes, sizeof(Vec4d) * vec_size * (se.temp_end - se.values_copy_end)) +  // vec_copy, same as temporaries
+            align_size(simd_bytes, sizeof(Vec4d) * vec_size * (se.values_copy_end - se.values_end)) + // vector values (probably not neccessary to allocate)
+            align_size(simd_bytes, sizeof(Vec4d) * se.constants_end ) +
+            align_size(simd_bytes, sizeof(Operation) * (sorted_nodes.size() + 64) );
+
+	// est *= 2;
+	// std::cout << "Estimated memory in processor: " << est << std::endl;
+
+    if (arena == nullptr)
+        arena = std::make_shared<ArenaAlloc>(simd_bytes, est);
+    else
+        BP_ASSERT(arena->size_ >= est);
+    return arena->create<Processor<Vec<Vec4d>>>(arena, se, vec_size);
+}
 
 inline ProcessorBase * ProcessorBase::create_processor(ExpressionDAG &se, uint vector_size,  uint simd_size, ArenaAllocPtr arena) {
 	if (simd_size == 0) {
@@ -925,7 +949,7 @@ inline ProcessorBase * ProcessorBase::create_processor(ExpressionDAG &se, uint v
 		} break;
 		case 4:
 		{
-			return create_processor_<Vec4d>(se, vector_size, simd_size, arena);
+			return create_processor_4(se, vector_size, simd_size, arena);
 		} break;
 		case 8:
 		{
