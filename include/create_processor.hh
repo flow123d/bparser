e8c732ae1597955b9a3538cb183e54c206a06ee6
KKSSainz <victor.trnka@gmail.com>
2022-12-09 11:28:15 +0100

    change filename

--- include/create_processor.hh
+++ include/create_processor.hh
@@ -4,10 +4,6 @@
 
 namespace bparser{
 
-    // uint get_simd_size();
-
-    // inline ProcessorBase * ProcessorBase::create_processor(details::ExpressionDAG &se, uint vector_size,  uint simd_size, ArenaAllocPtr arena);
-
     ProcessorBase * ProcessorBase::create_processor(ExpressionDAG &se, uint vector_size,  uint simd_size, ArenaAllocPtr arena) {
         if (simd_size == 0) {
             simd_size = get_simd_size();
@@ -31,24 +27,5 @@ namespace bparser{
                 return create_processor_<double>(se, vector_size, 1, arena);
             } break;
         }
-
-        // switch (simd_size) {
-        //     case 2:
-        //     {
-        //         return create_processor_SSE(se, vector_size, simd_size, arena);
-        //     } break;
-        //     case 4:
-        //     { 
-        //         return create_processor_AVX2(se, vector_size, simd_size, arena);
-        //     } break;
-        //     case 8:
-        //     {
-        //         return create_processor_AVX512(se, vector_size, simd_size, arena);
-        //     } break;
-        //     default:
-        //     {
-        //         return create_processor_double(se, vector_size, 1, arena);
-        //     } break;
-        // }
     }
 }
