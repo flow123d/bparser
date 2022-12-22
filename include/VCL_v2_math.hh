3f4ca898e5fff5d439b915f412a2624848926200
KKSSainz <victor.trnka@gmail.com>
2022-12-22 10:01:17 +0100

    put gcc pragmas in if for gnu only

--- include/VCL_v2_math.hh
+++ include/VCL_v2_math.hh
@@ -1,3 +1,6 @@
+//test if gnuc
+#ifdef __GNUC__
+
 // save diagnostic state
 #pragma GCC diagnostic push 
 
@@ -5,12 +8,20 @@
 #pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 
+// end if
+#endif
+
 // #include "vectorclass.h"
 
 #include "vectormath_exp.h"
 #include "vectormath_hyp.h"
 #include "vectormath_trig.h"
 
+//test if gnuc
+#ifdef __GNUC__
 
 // turn the warnings back on
 #pragma GCC diagnostic pop
+
+// end if
+#endif
--- include/processor_SSE.cc
+++ include/processor_SSE.cc
@@ -1,18 +1,32 @@
 #include "processor.hh"
 
+//test if gnuc
+#ifdef __GNUC__
+
 // save diagnostic state
 #pragma GCC diagnostic push 
 
 // turn off the specific warning.
 #pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+
+// end if
+#endif
 
 #undef MAX_VECTOR_SIZE
 #define MAX_VECTOR_SIZE 128
 
 #include "vectorclass.h"
 
+//test if gnuc
+#ifdef __GNUC__
+
 // turn the warnings back on
 #pragma GCC diagnostic pop
+
+// end if
+#endif
+
 
 namespace bparser{
 
