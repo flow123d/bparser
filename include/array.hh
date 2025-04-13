11fbb54cb4464f34829586114c8874eba89d5347
lukas-valenta-tul <lukas.valenta@tul.cz>
2025-04-13 19:31:48 +0200

    does not work, topic for consultation

--- include/array.hh
+++ include/array.hh
@@ -12,7 +12,6 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include <memory>
 #include <boost/math/constants/constants.hpp>
 #include "config.hh"
 #include "scalar_node.hh"
@@ -860,6 +859,46 @@ public:
 		return result;
 	}
 
+	static Eigen::MatrixX<details::ScalarWrapper> wrap_array(const bparser::Array& a) {
+		BP_ASSERT(a.shape().size() <= 2); // "Eigen does not support more than 2 dimensions"
+
+		using namespace details;
+		if (a.shape().size() == 0) {
+			return Eigen::MatrixX<ScalarWrapper>();
+		}
+		if (a.shape().size() == 1) {
+			Eigen::VectorX<ScalarWrapper> v(a.shape()[0]); //Rows x cols, which is which
+			for (uint i = 0; i < a.shape()[0]; i++) {
+				v(i) = ScalarWrapper(a.elements()[i]);
+			}
+			return v;
+		}
+		else {// (a.shape().size() == 2) {
+			MultiIdx index(a.range());
+			Eigen::MatrixX<ScalarWrapper> m(a.shape()[0], a.shape()[1]);
+			for (uint row = 0; row < a.shape()[0]; row++) {
+				for (uint col = 0; col < a.shape()[1]; col++) {
+					m(row, col) = ScalarWrapper(a[index]);
+					index.inc_src();
+				}
+			}
+			return m;
+		}
+	}
+
+	static bparser::Array unwrap_array(const Eigen::MatrixX<details::ScalarWrapper>& m) {
+		using namespace details;
+		Array a({ (uint)m.rows(), (uint)m.cols() });
+		MultiIdx index(a.range());
+		for (uint row = 0; row < a.shape()[0]; row++) {
+			for (uint col = 0; col < a.shape()[1]; col++) {
+				a.elements_[index.idx_src()] = m(row, col).get();
+				index.inc_src();
+			}
+		}
+		return a;
+	}
+
 	/**
 	 * Numpy.matmul:
 	 *
@@ -997,18 +1036,23 @@ public:
 	 */
 	static Array mat_mult(const Array &a,  const Array &b) {
 		//std::cout << "mat mult: " << print_vector(a.shape()) << " @ " << print_vector(b.shape()) << "\n";
-		Eigen::Matrix2d m1;
-		m1(0, 0) = 1;
-		m1(0, 1) = 2;
-		m1(1, 0) = 3;
-		m1(1, 1) = 4;
+		Eigen::Matrix<details::ScalarWrapper,2,2> m1;
+		m1(0, 0) = details::ScalarWrapper(details::ScalarNode::create_const(1));
+		m1(0, 1) = details::ScalarWrapper(details::ScalarNode::create_const(2));
+		m1(1, 0) = details::ScalarWrapper(details::ScalarNode::create_const(3));
+		m1(1, 1) = details::ScalarWrapper(details::ScalarNode::create_const(4));
 
-		Eigen::Vector2d v1;
-		v1(0) = 5;
-		v1(1) = 6;
+		Eigen::Vector<details::ScalarWrapper,2> v1;
 
-		std::cout << (m1 * v1) << std::endl;
-		std::cout << (v1.transpose() * m1) << std::endl;
+		v1(0) = details::ScalarWrapper(details::ScalarNode::create_const(5));
+		v1(1) = details::ScalarWrapper(details::ScalarNode::create_const(6));
+		//17,39
+		auto r = (m1 * v1);
+		//std::cout << r << std::endl;
+		//std::cout << (v1.transpose() * m1) << std::endl;
+		std::cout << "Shape: ---------" << std::endl;
+		std::cout << print_shape(a.shape()) << std::endl;
+		std::cout << print_shape(b.shape()) << std::endl;
 
 		if (a.shape().size() == 0)
 			Throw() << "Matmult can not multiply by scalar a." << "\n";
@@ -1016,14 +1060,15 @@ public:
 			Throw() << "Matmult can not multiply by scalar b." << "\n";
 
 		auto a_broadcast = MultiIdxRange(a.shape()).full();
-
 		if (a.shape().size() == 1) {
 			a_broadcast.insert_axis(0, 0, 1);
 		}
+
 		auto b_broadcast = MultiIdxRange(b.shape()).full();
 		if (b.shape().size() == 1) {
 			b_broadcast.insert_axis(1, 1, 1);
 		}
+
 		Shape a_shape = a_broadcast.source_shape_;
 		//uint a_only_dim = *(a_shape.end() - 2);
 		uint a_common_dim = *(a_shape.end() - 1);
@@ -1035,74 +1080,14 @@ public:
 		uint b_common_dim = *(b_shape.end() - 2);
 		b_shape.insert(b_shape.end() - 2, 1);
 		// b_shape : (...,i,j,1,l,m)
+
 		if (a_common_dim != b_common_dim)
 			Throw() << "Matmult summing dimension mismatch: " << a_common_dim << " != " << b_common_dim << "\n";
-		Shape common_shape = MultiIdxRange::broadcast_common_shape(a_shape, b_shape);
+		//Shape common_shape = MultiIdxRange::broadcast_common_shape(a_shape, b_shape);
 		// common_shape : (...,i,j,k,l,m)
-		Shape result_shape(common_shape);
-		*(result_shape.end() - 2) = 1;
-		// result_shape : (...,i,j,k,1,m)
-
-		MultiIdxRange a_range = MultiIdxRange(a_shape).full().broadcast(common_shape);
-		a_range.target_transpose(-2, -1);
-		MultiIdxRange b_range = MultiIdxRange(b_shape).full().broadcast(common_shape);
-		b_range.target_transpose(-2, -1);
-		MultiIdxRange result_range = MultiIdxRange(result_shape).full();
-		result_range.target_transpose(-2, -1);
-		// transpose a_range and b_range in  order to have common_dim the last one:
-		// a_shape : (....i,j, k,l,1)
-		// b_shape : (...,i,j,1,m,l)
-		*(b_range.target_transpose_.end() - 1) = b_range.target_transpose_.size() - 2;
-		*(b_range.target_transpose_.end() - 2) = b_range.target_transpose_.size() - 1;
-
-
-		MultiIdx a_idx(a_range);
-		MultiIdx b_idx(b_range); // allocated
-		MultiIdx result_idx(result_range);
-/*
-		std::cout << "a_idx, shp: " << print_vector(a_idx.range_.full_shape_) << "\n";
-		std::cout << "b_idx, shp: " << print_vector(b_idx.range_.full_shape_) << "\n";
-		std::cout << "r_idx, shp: " << print_vector(result_idx.range_.full_shape_) << "\n";
-*/
-
-		ScalarNodePtr sum;
-		Array result(result_shape);
-		for(;result_idx.valid();) {
-			sum = nullptr;
-			a_idx.reset_indices(result_idx);
-			b_idx.reset_indices(result_idx);
-			for(;a_idx.valid();) {
-/*
-				std::cout << "a_idx: " << print_vector(a_idx.indices()) << " didx: "
-						<< a_idx.src_idx() << "\n";
-				std::cout << "b_idx: " << print_vector(b_idx.indices()) << " didx: "
-						<< b_idx.src_idx() << "\n";
-*/
-				ScalarNodePtr mult = details::ScalarNode::create<details::_mul_>(
-						a.elements_[a_idx.idx_src()],
-						b.elements_[b_idx.idx_src()]);
-				if (sum == nullptr) {
-					sum = mult;
-				} else {
-					// TODO: how to use inplace operations correctly ??
-					sum = (details::ScalarWrapper(sum) + details::ScalarWrapper(mult)).get();
-				}
-				//std::cout << "aidx ";
-				a_idx.inc_trg(-1,1, false);
-				//std::cout << "bidx ";
-				b_idx.inc_trg(-1,1, false);
-				BP_ASSERT(a_idx.valid() == b_idx.valid());
-			}
-/*
-			std::cout << "r_idx: " << print_vector(result_idx.indices()) << " didx: "
-									<< result_idx.src_idx() << "\n";
-*/
-
-			result.elements_[result_idx.idx_src()] = sum;
-
-			result_idx.inc_trg(-2);
-
-		}
+		
+		Array result = unwrap_array(wrap_array(a) * wrap_array(b));
+		Shape result_shape = result.shape();
 
 
 		auto final_range = MultiIdxRange(result.shape()).full();
--- include/scalar_wrapper.hh
+++ include/scalar_wrapper.hh
@@ -15,16 +15,46 @@
 
 namespace bparser {
 	namespace details {
+		// Eigen compatible wrapper for ScalarNode
 		struct ScalarWrapper {
 
+			ScalarWrapper() : node(ScalarNode::create_zero()) { ; }
+			ScalarWrapper(int i) : node(ScalarNode::create_const(i)) { ; }
+			ScalarWrapper(double d) : node(ScalarNode::create_const(d)) { ; }
 			ScalarWrapper(ScalarNodePtr existing_ptr) : node(existing_ptr) { ; }
 
-
-			inline ScalarWrapper operator+(const ScalarWrapper& b) const {
-				return ScalarWrapper(ScalarNode::create<_add_>(node, b.node));
+			inline ScalarWrapper operator-() const {
+				return un_op<_minus_>(*this);
 			}
 
-			inline ScalarNodePtr operator*() const {
+			inline ScalarWrapper& operator+=(const ScalarWrapper& b) {
+				node = bin_op<_add_>(*this, b).get();
+				return *this;
+			}
+
+			inline ScalarWrapper operator+(const ScalarWrapper& b) const {
+				return bin_op<_add_>(*this, b);
+			}
+
+			inline ScalarWrapper operator-(const ScalarWrapper& b) const {
+				return bin_op<_sub_>(*this, b);
+			}
+
+			inline ScalarWrapper operator*(const ScalarWrapper& b) const {
+				return bin_op<_mul_>(*this, b);
+			}
+
+			inline ScalarWrapper operator/(const ScalarWrapper& b) const {
+				return bin_op<_div_>(*this, b);
+			}
+
+			inline bool operator==(const ScalarWrapper& b) const {
+				b.get();
+				return false;// return bin_op<_eq_>(*this, b); //How??????????
+			}
+
+
+			inline ScalarNodePtr operator*() const { //dereference
 				return get();
 			}
 
@@ -32,15 +62,58 @@ namespace bparser {
 				return node;
 			}
 
+			template<class T>
+			static ScalarWrapper bin_op(const ScalarWrapper& a, const ScalarWrapper& b) {
+				return ScalarWrapper(ScalarNode::create<T>(a.get(), b.get()));
+			}
+
+			template<class T>
+			static ScalarWrapper un_op(const ScalarWrapper& a) {
+				return ScalarWrapper(ScalarNode::create<T>(a.get()));
+			}
 
 
 		protected:
 			ScalarNodePtr node;
+
+
+		}; //ScalarWrapper
+
+		//inline std::ostream& operator<<(std::ostream& out, const ScalarWrapper& s) {
+		//
+		//}
+
+#define UN_OP(OP)											\
+		inline ScalarWrapper OP(const ScalarWrapper& s) {	\
+			return ScalarWrapper::un_op<_##OP##_>(s);		\
+		}													\
+		using std::OP;
+
+		inline ScalarWrapper abs(const ScalarWrapper& s) {
+			return ScalarWrapper::un_op<_abs_>(s); 
+		}
+		using std::abs;
+
+		//https://eigen.tuxfamily.org/dox/namespaceEigen.html#a54cc34b64b4935307efc06d56cd531df
+		inline ScalarWrapper abs2(const ScalarWrapper& s) {
+			return s*s;
 		};
 
+		inline ScalarWrapper sqrt(const ScalarWrapper& s) {
+			return ScalarWrapper::un_op<_sqrt_>(s);
+		}
+		using std::sqrt;
 
-	}
-}
+		UN_OP(log)
+		UN_OP(log10)
+
+
+
+
+
+	} //details
+} //bparser
+
 //https://eigen.tuxfamily.org/dox/structEigen_1_1NumTraits.html
 namespace Eigen {
 	template<> struct NumTraits<bparser::details::ScalarWrapper>
@@ -54,7 +127,7 @@ namespace Eigen {
 			IsComplex = 0,
 			IsInteger = 0,
 			IsSigned = 1,
-			RequireInitialization = 0,
+			RequireInitialization = 1,
 			ReadCost = HugeCost,
 			AddCost = HugeCost,
 			MulCost = HugeCost
